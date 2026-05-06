54afb34a3c50401fa8dbbbc7587dba5ac6b0a40d
ZDragon <164170132+ZDragon098@users.noreply.github.com>
2026-05-06 08:51:56 +0700

    Add: two-integer-sum-ii - submission-9

---
+++ Data Structures & Algorithms/two-integer-sum-ii/submission-9.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& numbers, int target) {
+        vector<int> ans;
+        int i = 0, j = numbers.size() - 1;
+        while(i < j){
+            if(numbers[i] + numbers[j] == target){
+                return {i+1,j+1};
+            }
+            else if(numbers[i] + numbers[j] < target) i++;
+            else j--;
+        }
+         return {-1,-1};
+    }
+// private:
+//     int binarySearch(vector<int> a, int l, int r, int x){
+//         while(l < r){
+//             int m = (l + r) / 2;
+//             if(a[m] == x) return m;
+//             else if(a[m] > x) r = m - 1;
+//             else l = m + 1;
+//         }
+//         return -1;
+//     }
+};
