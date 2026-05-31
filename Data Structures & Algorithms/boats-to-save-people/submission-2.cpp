c8831f3771d2c61650f1fbf49c70b03278032992
ZDragon <164170132+ZDragon098@users.noreply.github.com>
2026-05-31 08:10:49 +0700

    Add: boats-to-save-people - submission-2

---
+++ Data Structures & Algorithms/boats-to-save-people/submission-2.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int numRescueBoats(vector<int>& people, int limit) {
+        sort(people.begin(),people.end());
+        int l = 0, res = 0,r = people.size()-1;
+        while(l<=r){
+            int temp = limit - people[r--];
+            res++;
+            if(l<=r && temp >= people[l]) l++;
+        }
+        return res;
+    }
+};
