9f970c80974b3da645aadc8944b6f006beefc26c
Long PTIT <164170132+ZDragon098@users.noreply.github.com>
2026-07-11 23:02:33 +0700

    Add: find-duplicate-integer - submission-2

---
+++ Data Structures & Algorithms/find-duplicate-integer/submission-2.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        for(int num: nums){
+            int idx = abs(num);
+            if(nums[idx] < 0) return abs(num);
+            nums[idx] *= -1;
+        }
+    }
+};
